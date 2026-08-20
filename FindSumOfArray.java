949295964cc9c91a8bf8a7ed1f210a62c038d4c2
Jaya sharma <imsharmajaya@gmail.com>
2026-08-20 20:40:25 +0530

    Add files via upload

---
+++ FindSumOfArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FindSumOfArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int arr[] = new int[n];
+    
+        int  sum = 0;
+        for(int i =0;i<arr.length;i++)
+        sum = sum + arr[i];
+
+
+    }
+    System.out.println("Sum = " + sum);
+    
+}
